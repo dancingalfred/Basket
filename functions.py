4b054a9a44f0405d394e820539b4a6e35b1a2d02
dancingalfred <robban021@hotmail.com>
2022-11-25 21:48:14 +0100

    working score and teamname scrape

---
+++ functions.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+def convertWebpageToSoup(website):
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, "lxml")
+    return soup
+
+def getHomeTeamInfo(soup):
+    tag = soup.body
+    homeTeamInfo = tag.find(class_="home-wrapper team-box")
+    homeTeamName = homeTeamInfo.find(class_="name")
+    homeTeamName = homeTeamName.text
+    homeTeamName = homeTeamName.strip()
+
+
+    homeTeamScore = homeTeamInfo.find(class_="score")
+    homeTeamScore = homeTeamScore.text
+    homeTeamScore = homeTeamScore.strip()
+    return homeTeamName, homeTeamScore
+
+
+def getAwayTeamInfo(soup):
+    tag = soup.body
+    awayTeamInfo = tag.find(class_="away-wrapper team-box")
+
+    awayTeamName = awayTeamInfo.find(class_="name")
+    awayTeamName = awayTeamName.text
+    awayTeamName = awayTeamName.strip()
+
+    awayTeamScore = awayTeamInfo.find(class_="score")
+    awayTeamScore = awayTeamScore.text
+    awayTeamScore = awayTeamScore.strip()
+    return awayTeamName, awayTeamScore
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from functions import getHomeTeamInfo, getAwayTeamInfo, convertWebpageToSoup
 
 gameId = 2110419
 otherId = 34106
@@ -8,32 +7,8 @@ user = 597
 
 website = f"https://hosted.dcd.shared.geniussports.com/SBF/en/competition/{otherId}/match/{gameId}/summary"
 
-result = requests.get(website)
+soup = convertWebpageToSoup(website)
+homeTeamName,homeTeamScore = getHomeTeamInfo(soup)
+awayTeamName,awayTeamScore = getAwayTeamInfo(soup)
 
-content = result.text
-
-soup = BeautifulSoup(content, "lxml")
-print(soup.prettify())
-
-body = soup.find("body", dir="ltr", class_=f"_user_{user} _comp_{otherId} ltr  summary")
-body = body.find("div", class_="hs-page hs-container xs-hs-container")
-body = body.find("div", class_="hs-container noresize main-container")
-body = body.find("div", class_="hs-page hs-container xs-hs-container")
-body = body.find("div", class_="hs-row")
-body = body.find("div", class_="hs-col-full")
-body = body.find("div", class_=f"extfix_{gameId}")
-body = body.find("div", class_="match-page page")
-
-
-
-
-
-matchDetails = body.find("div", class_="BLOCK_MATCH_HEADER")
-
-homeInfo = matchDetails.find("div", class_="home-wrapper team-box")
-homeTeamScore = homeInfo.find("div", class_="score").get_text()
-
-awayInfo = matchDetails.find("div", class_="home-wrapper team-box")
-awayTeamScore = awayInfo.find("div", class_="score").get_text()
-
-print(f"Hemmalag: {homeTeamScore} - Bortalag: {awayTeamScore} ")
+print(f"{homeTeamName}: {homeTeamScore} - {awayTeamName}: {awayTeamScore} ")
